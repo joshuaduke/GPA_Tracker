3c9cfa1cdd6523c9dce942a05eb6c5097ff0cc07
Joshua Duke <dukejoshuae@gmail.com>
2020-03-03 19:43:11 -0500

    Repositioned course routes remaking semester routes in order to implement course routes

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.get("/semesters/new", (req, res)=>{
     if(err){
       console.log(err);
     } else{
-      res.render("semesters/semestersNew", {semesters: allSemesters})
+      res.render("semesters/new", {semesters: allSemesters})
     }
   });
 });
@@ -69,157 +69,49 @@ app.post("/semesters", (req, res)=>{
   })
 });
 
-  // SHOW SEMESTER ROUTE -- Show which courses belong to 1 semester
-app.get("/semesters/:id", (req, res)=>{
-  //find semester with provided ID
-  Semester.findById(req.params.id).populate("courses").exec((err, foundSemester)=>{
-    if(err){
-      console.log(err)
-    } else {
-      console.log(foundSemester);
-      res.render("courses/index", {semester: foundSemester});
-    }
-  })
-});
+  // SHOW SEMESTER ROUTE -- display all courses
+  app.get("/semesters/:id", (req, res)=>{
+    //find semester with provided ID
+    Semester.find(req.params.id, (err, foundSemester)=>{
+      if(err){
+        console.log(err)
+      } else {
+        res.render("semesters/show", {semester: foundSemester});
+      }
+    })
+  });
 
-  //EDIT SEMESTER ROUTE -- Edit form for a semester
-app.get("/semesters/:id/edit", (req , res)=>{
 
-}); 
+  // EDIT SEMESTER ROUTE -- Show edit form for a semester
+  app.get("/semesters/:id/edit", (req , res)=>{
+    Semester.find({}, (err, allSemesters)=>{
+      if(err){
+        console.log(err);
+      } else {
+        res.render("semesters/edit", {semesters: allSemesters});
+      }
+    });
+  }); 
+
+  // UPDATE SEMESTER ROUTE -- Update a semester then redirect PUT
+  app.put("/semesters/:id/", (req , res)=>{
+
+  }); 
+  // DESTROY SEMESTER ROUTE -- Delete a dog then redirect
+  app.delete("/semesters/:id", (req , res)=>{
+
+  }); 
 
 // ====================================
 // ------ REST ROUTES FOR COURSES -----
 // ====================================
 
-// app.get("/semesters", (req, res)=>{
-//   Semester.find({}, (err, allSemesters)=>{
-//     if(err){
-//       console.log("Main Route Error");
-//       console.log("err");
-//     } else {
-//       res.render("semesters/semestersIndex", {semesters: allSemesters});
-//     }
-//   }).sort({year: 'desc'}).exec((err, docs)=>{
-//     if(err){
-//       console.log(err);
-//     }
-//   });
-// });
+// INDEX ROUTE
 
-//INDEX ROUTE -- display all courses
-app.get("/semesters/:id/courses", (req, res)=>{
-  Semester.findById(req.params.id, (err, semester)=>{
-    if(err){
-      console.log(err);
-    } else{
-      res.render("courses/index", {semester: semester})
-    }
-  });
-});
-
-//NEW COURSES ROUTE -- display delete option for courses
-// delete this route and change for javascript functionality
-app.get("/courses/new", (req, res)=>{
-  Course.find({}, (err, allCourses)=>{
-    if(err){
-      console.log(err);
-    } else{
-      res.render("courses/new", {courses: allCourses})
-    }
-  });
-});
-
-//NEW COURSES ROUTE -- display new course form
-app.get("/courses/newCourses", (req, res)=>{
-  Course.find({}, (err, allCourses)=>{
-    if(err){
-      console.log(err);
-    } else{
-      res.render("courses/newCourses", {courses: allCourses})
-    }
-  });
-});
-
-//CREATE ROUTE -- add new course
-app.post("/courses", (req, res)=>{
-  //get data from form and add to courses array
-  let name = req.body.name;
-  let courseGrade = req.body.courseGrade;
-  let newCourses = {name:name , courseGrade:courseGrade};
-
-  //create a new course and save to database
-  Course.create(newCourses, (err, newlyCreated)=>{
-    if(err){
-      console.log("Create route error");
-    } else {
-      //redirect back to courses
-      res.redirect("/courses");
-    }
-  })
+app.get("semesters/:id/courses", (req, res)=>{
+  res.send("Create index route");
 }); 
 
-//SHOW ROUTE 
-app.get("/courses/:id", (req, res)=>{
-  Course.findById(req.params.id, (err, foundCourse)=>{
-    if(err){
-      console.log("Show route error");
-      console.log(err);
-    } else {
-      console.log(foundCourse);
-      res.render("courses/show", {course: foundCourse});
-    }
-  });
-});
-
-//EDIT ROUTE - Edit a specific course
-app.get("/courses/:id/edit", (req, res)=>{
-  Course.findById(req.params.id, (err, foundCourse)=>{
-    if(err){
-      res.redirect("/courses");
-    } else {
-      res.render("courses/edit", {course:foundCourse});
-    }
-  });
-});
-
-//UPDATE ROUTE
-app.put("/courses/:id", (req, res)=>{
- Course.findByIdAndUpdate(req.params.id ,req.body.course, (err, updatedCourse)=>{
-  if(err){
-    console.log("UPDATE ROUTE Error");
-  } else {
-    res.redirect("/courses/" + req.params.id);
-  }
- });
-});
-
-//DELETE ROUTE
-app.delete("/courses/:id", (req, res)=>{
-
-  Course.findByIdAndDelete(req.params.id, (err)=>{
-    if(err){
-      console.log("Delete Error");
-      console.log(err);
-      res.redirect("/courses");
-    } else {
-      res.redirect("/courses/new");
-    }
-  });
-});
-
-// ----- REST ROUTES FOR GRADED WORK -----
-
-//NEW GRADE ROUTE -- add a new Grade
-app.get("/courses/newGrade", (req, res)=>{
-  // Course.find({}, (err, allCourses)=>{
-  //   if(err){
-  //     console.log(err);
-  //   } else{
-  //     res.render("newGrade", {courses: allCourses})
-  //   }
-  // });
-  res.render("courses/newGrade")
-});
 
 app.listen("3000", process.env.PORT, ()=>{
   console.log("This server has started");
--- courseRoute.js
+++ courseRoute.js
@@ -132,3 +132,160 @@ app.listen("3000", process.env.PORT, ()=>{
 //     console.log(course);
 //   }
 // });
+
+
+//SHOW PAGE
+
+<main>
+  <div class="container_nav">
+    <ul>
+      <li><a href="/courses"> Courses </a></li>
+    </ul>
+    <ul>
+      <li><a href="/courses/<%=semester._id %>/edit">Edit </a></li>
+    </ul>
+  </div>
+
+  <div id="courses_container">
+    <p> <%= semester.name %></p>
+    <% console.log("=========== SHOW PAGE ==========") %>
+    <% console.log(courses) %>
+
+    <% courses.forEach((course)=>{ %>
+
+      <div>
+        <a class="card" href="/semesters/<%=semester._id%>/course">
+          <p class="course_name">
+            <i class="fas fa-school"></i> 
+            <%= semester.name %>
+            
+          </p> 
+          <p><span><%= semester.courseGrade %> </span> % </p> 
+        </a>
+      </div>
+      
+    <% }) %>
+  </div>
+</main>
+
+
+
+// ====================================
+// ------ REST ROUTES FOR COURSES -----
+// ====================================
+
+//SHOW ROUTE -- display a course
+app.get("/semesters/:id/course/:id", (req, res)=>{
+  Course.findById(req.params.id, (err, foundSemester)=>{
+    console.log(foundSemester);
+    if(err){
+      console.log(err);
+    } else{
+      res.render("courses/show", {semester: foundSemester})
+
+    }
+  });
+});
+
+//NEW COURSES ROUTE -- display delete option for courses
+// delete this route and change for javascript functionality
+app.get("/courses/new", (req, res)=>{
+  Course.find({}, (err, allCourses)=>{
+    if(err){
+      console.log(err);
+    } else{
+      res.render("courses/new", {courses: allCourses})
+    }
+  });
+});
+
+//NEW COURSES ROUTE -- display new course form
+app.get("/courses/newCourses", (req, res)=>{
+  Course.find({}, (err, allCourses)=>{
+    if(err){
+      console.log(err);
+    } else{
+      res.render("courses/newCourses", {courses: allCourses})
+    }
+  });
+});
+
+//CREATE ROUTE -- add new course
+app.post("/courses", (req, res)=>{
+  //get data from form and add to courses array
+  let name = req.body.name;
+  let courseGrade = req.body.courseGrade;
+  let newCourses = {name:name , courseGrade:courseGrade};
+
+  //create a new course and save to database
+  Course.create(newCourses, (err, newlyCreated)=>{
+    if(err){
+      console.log("Create route error");
+    } else {
+      //redirect back to courses
+      res.redirect("/courses");
+    }
+  })
+}); 
+
+//SHOW ROUTE 
+app.get("/courses/:id", (req, res)=>{
+  Course.findById(req.params.id, (err, foundCourse)=>{
+    if(err){
+      console.log("Show route error");
+      console.log(err);
+    } else {
+      res.render("courses/show", {course: foundCourse});
+    }
+  });
+});
+
+//EDIT ROUTE - Edit a specific course
+app.get("/courses/:id/edit", (req, res)=>{
+  Course.findById(req.params.id, (err, foundCourse)=>{
+    if(err){
+      res.redirect("/courses");
+    } else {
+      res.render("courses/edit", {course:foundCourse});
+    }
+  });
+});
+
+//UPDATE ROUTE
+app.put("/courses/:id", (req, res)=>{
+ Course.findByIdAndUpdate(req.params.id ,req.body.course, (err, updatedCourse)=>{
+  if(err){
+    console.log("UPDATE ROUTE Error");
+  } else {
+    res.redirect("/courses/" + req.params.id);
+  }
+ });
+});
+
+//DELETE ROUTE
+app.delete("/courses/:id", (req, res)=>{
+
+  Course.findByIdAndDelete(req.params.id, (err)=>{
+    if(err){
+      console.log("Delete Error");
+      console.log(err);
+      res.redirect("/courses");
+    } else {
+      res.redirect("/courses/new");
+    }
+  });
+});
+
+// ----- REST ROUTES FOR GRADED WORK -----
+
+//NEW GRADE ROUTE -- add a new Grade
+app.get("/courses/newGrade", (req, res)=>{
+  // Course.find({}, (err, allCourses)=>{
+  //   if(err){
+  //     console.log(err);
+  //   } else{
+  //     res.render("newGrade", {courses: allCourses})
+  //   }
+  // });
+  res.render("courses/newGrade")
+});
